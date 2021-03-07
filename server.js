17cf8a4359e17996038d2c1f144c4cb83cae453d
bstanfield1992 <bstanfield1992@gmail.com>
2021-03-07 13:06:46 -0700

    took out password for now

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// Dependencies
+// Dependencies and DB connection
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 const conTable = require("console.table");
@@ -7,7 +7,15 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3000,
     user: "root",
-    password: "PW4bstanfi!",
+    password: "root",
     database: "employee_trackerDB"
+});
+
+
+// connect ID
+connection.connect((err) => {
+    if (err) throw err
+    console.log("Connected as Id" + connection.threadId)
+    startPrompt();
 });
 
